6c4418374fb5a455f44cdd4dc191f955fa277db2
Marvelbad <badra.aqua@gmail.com>
2025-03-02 20:33:04 +0300

    Linux course

--- src/ru/javarush/system/linux/level01/task11/solution.sh
+++ src/ru/javarush/system/linux/level01/task11/solution.sh
@@ -2,10 +2,10 @@
 mkdir -p project/src project/docs
 
 # Создаем файл readme.txt в папке docs
-touch project/src/docs/readme.txt
+touch project/docs/readme.txt
 
 # Копируем файл readme.txt из папки docs в папку src
 cp project/docs/readme.txt project/src/
 
 # Перемещаем файл readme.txt из папки src в корневую папку project
-mv project/sec/readme.txt project
+mv project/src/readme.txt project
