84034b37758534509e26ba5b0e837a9f244935dd
Marvelbad <badra.aqua@gmail.com>
2025-09-03 19:20:22 +0300

    Task 02

---
+++ src/ru/javarush/java/core/level02/task02/Solution.java
@@ -0,0 +1,13 @@
+package ru.javarush.java.core.level02.task02;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем и инициализируем переменные в одной строке
+        int firstRoll = 7;
+        int secondRoll = 3;
+        int totalScore = firstRoll + secondRoll;
+
+        // Выводим значение переменной totalScore на экран
+        System.out.println(totalScore);
+    }
+}
