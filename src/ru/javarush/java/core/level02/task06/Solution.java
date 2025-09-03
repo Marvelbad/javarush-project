80b2deacd59a12b0467a4a45e89b378342df1af0
Marvelbad <badra.aqua@gmail.com>
2025-09-03 19:32:34 +0300

    Task 06

---
+++ src/ru/javarush/java/core/level02/task06/Solution.java
@@ -0,0 +1,14 @@
+package ru.javarush.java.core.level02.task06;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменные и присваиваем им значения
+        String firstName = "Luke";
+        String lastName = "Skywalker";
+        // Объединяем имя и фамилию через пробел
+        String result = firstName + " " + lastName;
+
+        // Выводим получившееся полное имя
+        System.out.println(result);
+    }
+}
