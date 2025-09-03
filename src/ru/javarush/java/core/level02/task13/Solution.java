89f502154c271050ad36e9dcbffbaffdf763c961
Marvelbad <badra.aqua@gmail.com>
2025-09-03 20:33:14 +0300

    Task 13

---
+++ src/ru/javarush/java/core/level02/task13/Solution.java
@@ -0,0 +1,17 @@
+package ru.javarush.java.core.level02.task13;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменные с информацией о персонаже
+        byte level = 64;
+        int goldAmount = 28;
+        double rating = 2.8;
+        String characterName = "Max";
+
+        // Выводим все данные на экран
+        System.out.println(level);
+        System.out.println(goldAmount);
+        System.out.println(rating);
+        System.out.println(characterName);
+    }
+}
