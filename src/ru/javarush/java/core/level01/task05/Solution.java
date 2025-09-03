065ea8c26c99bf77c99693283be20cb17a6cbfd8
Marvelbad <badra.aqua@gmail.com>
2025-09-03 18:31:02 +0300

    Task 05

---
+++ src/ru/javarush/java/core/level01/task05/Solution.java
@@ -0,0 +1,11 @@
+package ru.javarush.java.core.level01.task05;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменную типа String с именем myMessage
+        String myMessage = "I ❤\uFE0F YOU";
+
+        // Выводим значение переменной myMessage на экран
+        System.out.println(myMessage);
+    }
+}
