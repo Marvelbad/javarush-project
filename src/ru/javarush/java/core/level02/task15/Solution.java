e2efd9704b707111b6073526160b45ede7b84d78
Marvelbad <badra.aqua@gmail.com>
2025-09-03 20:40:24 +0300

    Task 15

---
+++ src/ru/javarush/java/core/level02/task15/Solution.java
@@ -0,0 +1,16 @@
+package ru.javarush.java.core.level02.task15;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменные разных типов и присваиваем им произвольные значения
+        byte byteValue = 16;
+        int intValue = 28;
+        double doubleValue = 28.8;
+
+        // Выводим информацию о размере каждого типа данных
+        System.out.println("byte — 1 байт");
+        System.out.println("int — 4 байта");
+        System.out.println("double — 8 байтов");
+
+    }
+}
