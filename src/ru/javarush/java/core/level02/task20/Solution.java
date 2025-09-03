ceaa1d90cd2ce17a7db2b7e03fdf18dd9476331e
Marvelbad <badra.aqua@gmail.com>
2025-09-03 20:56:14 +0300

    Task 20

---
+++ src/ru/javarush/java/core/level02/task20/Solution.java
@@ -0,0 +1,19 @@
+package ru.javarush.java.core.level02.task20;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем объект Scanner для чтения данных с консоли
+        Scanner scanner = new Scanner(System.in);
+
+        // Считываем имя пользователя (строка)
+        String name = scanner.nextLine();
+
+        // Считываем возраст пользователя (целое число)
+        int age = scanner.nextInt();
+
+        // Выводим отформатированную визитную карточку
+        System.out.println("Меня зовут " + name + ", " + "мне " + age + " лет.");
+    }
+}
