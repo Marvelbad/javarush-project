b6d91ed45a34ccf1e1e360ec69b83aa7fc0625c3
Marvelbad <badra.aqua@gmail.com>
2025-09-03 20:24:46 +0300

    Task 11

---
+++ src/ru/javarush/java/core/level02/task11/Solution.java
@@ -0,0 +1,14 @@
+package ru.javarush.java.core.level02.task11;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем строковую переменную, содержащую год выпуска фильма
+        String strNum = "1999";
+
+        // Преобразуем строку в числовой тип int
+        int numbers = Integer.valueOf(strNum);
+
+        // Выводим числовое значение года на экран
+        System.out.println(numbers);
+    }
+}
