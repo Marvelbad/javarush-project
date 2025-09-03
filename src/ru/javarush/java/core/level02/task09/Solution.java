29698aca16270615eacfc1038dc9068b295c2810
Marvelbad <badra.aqua@gmail.com>
2025-09-03 20:12:19 +0300

    Task 09

---
+++ src/ru/javarush/java/core/level02/task09/Solution.java
@@ -0,0 +1,14 @@
+package ru.javarush.java.core.level02.task09;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменную типа int и присваиваем ей произвольное целое значение (код доступа)
+        int value = 23;
+
+        // Преобразуем целочисленное значение accessCode в строку
+        String toString = String.valueOf(value);
+
+        // Выводим полученную строку на экран
+        System.out.println(toString);
+    }
+}
