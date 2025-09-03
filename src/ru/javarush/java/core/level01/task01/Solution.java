4b34aeddc201a76e1bfd1a15c02d125078fe6a2a
Marvelbad <badra.aqua@gmail.com>
2025-09-03 18:13:53 +0300

    Task 1

---
+++ src/ru/javarush/java/core/level01/task01/Solution.java
@@ -0,0 +1,7 @@
+package ru.javarush.java.core.level01.task01;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Круто быть программистом!😎");
+    }
+}
