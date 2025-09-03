8be7b3e69ca9c70363c067ec362a66857ebf65c5
Marvelbad <badra.aqua@gmail.com>
2025-09-03 18:16:51 +0300

    Task 02

---
+++ src/ru/javarush/java/core/level01/task02/Solution.java
@@ -0,0 +1,12 @@
+package ru.javarush.java.core.level01.task02;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Выводим текущий год 2025 на отдельной строке
+        System.out.println(2025);
+        System.out.println("\uD83D\uDE05");
+
+
+
+    }
+}
