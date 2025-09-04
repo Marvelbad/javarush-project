6ed28bd8ba2f2f4aeda3a6c68c9a5584d362ce5b
Marvelbad <badra.aqua@gmail.com>
2025-09-04 11:43:12 +0300

    Task 10

---
+++ src/ru/javarush/java/core/level03/task10/Solution.java
@@ -0,0 +1,15 @@
+package ru.javarush.java.core.level03.task10;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем и присваиваем счёт нашей команды и соперника
+        int ourScore = 5;
+        int opponentScore = 3;
+
+        // weWon — результат логического сравнения: true, если наш счёт больше
+        boolean weWon = ourScore > opponentScore;
+
+        // Выводим значение переменной weWon на экран
+        System.out.println(weWon);
+    }
+}
