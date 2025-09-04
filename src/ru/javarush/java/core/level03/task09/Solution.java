9ba610b8dfa03fdf58e49785470bd33ebecb43bf
Marvelbad <badra.aqua@gmail.com>
2025-09-04 11:40:38 +0300

    Task 09

---
+++ src/ru/javarush/java/core/level03/task09/Solution.java
@@ -0,0 +1,11 @@
+package ru.javarush.java.core.level03.task09;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменную типа boolean с именем isJavaFun
+        boolean isJavaFun = true;
+
+        // Выводим значение переменной isJavaFun на экран
+        System.out.println(isJavaFun);
+    }
+}
