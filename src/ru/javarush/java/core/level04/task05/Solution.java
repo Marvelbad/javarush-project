0a2e1d2bd46c26121ac74eb08f522e9390aa5d35
Marvelbad <badra.aqua@gmail.com>
2025-09-07 22:56:13 +0300

    Task 05

---
+++ src/ru/javarush/java/core/level04/task05/Solution.java
@@ -0,0 +1,13 @@
+package ru.javarush.java.core.level04.task05;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Цикл for для обратного отсчёта от 5 до 1
+        for (int i = 5; i >= 1; i--) {
+            // Вывод текущего числа на новой строке
+            System.out.println(i);
+        }
+        // Вывод поздравления после завершения цикла
+        System.out.println("С Новым годом!");
+    }
+}
