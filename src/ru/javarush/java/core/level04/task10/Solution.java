824a67ffe59cb40cb61a7b1b3963208c0c514ab6
Marvelbad <badra.aqua@gmail.com>
2025-09-08 22:21:03 +0300

    Task 10

---
+++ src/ru/javarush/java/core/level04/task10/Solution.java
@@ -0,0 +1,23 @@
+package ru.javarush.java.core.level04.task10;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем Scanner для чтения данных с клавиатуры
+        Scanner console = new Scanner(System.in);
+
+        int pin;
+        // Используем do-while, чтобы запросить PIN хотя бы один раз
+        // и повторять ввод, пока число не будет в диапазоне 1000..9999
+        do {
+            // Запрос ввода PIN-кода
+            System.out.println("Введите пин-код:");
+            // читаем целое число
+            pin = console.nextInt();
+        } while (pin < 1000 || pin > 9999);
+
+        // Выводим сообщение о принятии PIN-кода
+        System.out.println("PIN-код принят");
+    }
+}
