39d4c15c3b075b7adcf89556926b2f065796c10a
Marvelbad <badra.aqua@gmail.com>
2025-10-02 15:57:17 +0300

    Task 02

---
+++ src/ru/javarush/java/core/level06/task02/Solution.java
@@ -0,0 +1,13 @@
+package ru.javarush.java.core.level06.task02;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменную dragonHoard типа long и присваиваем ей 10 миллиардов
+        long dragonHoard = 10_000_000_000L;
+        // Используем суффикс L и подчёркивания для удобства чтения
+
+        
+        // Выводим значение переменной на экран
+        System.out.println(dragonHoard);
+    }
+}
