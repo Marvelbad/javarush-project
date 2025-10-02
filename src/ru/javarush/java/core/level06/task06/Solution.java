4f3d9db5a9381940ae14f7e67acd676dc480518e
Marvelbad <badra.aqua@gmail.com>
2025-10-02 16:20:23 +0300

    Task 06

---
+++ src/ru/javarush/java/core/level06/task06/Solution.java
@@ -0,0 +1,21 @@
+package ru.javarush.java.core.level06.task06;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем объект Scanner для считывания данных с клавиатуры
+        Scanner scanner = new Scanner(System.in);
+
+        // Просим пользователя ввести сумму платежа
+        System.out.println("Введите сумму:");
+
+
+        // Считываем введенное значение и сохраняем его в переменную типа double
+
+        double customerPayment = scanner.nextDouble();
+
+        // Подтверждаем полученную сумму, выводя её на экран
+        System.out.println("Внесено средств: " + customerPayment);
+    }
+}
