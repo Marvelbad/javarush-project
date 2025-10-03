5293b789ed8bdc292ad8b799b1fbeb89abd1381a
Marvelbad <badra.aqua@gmail.com>
2025-10-03 23:38:04 +0300

    Task 09

---
+++ src/ru/javarush/java/core/level07/task09/Solution.java
@@ -0,0 +1,20 @@
+package ru.javarush.java.core.level07.task09;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем двумерный массив theaterSeating размером 2 строки и 3 столбца
+        int[][] theaterSeating = new int[2][3];
+
+        // Заполняем массив числами от 1 до 6 по строкам
+        // счётчик для присвоения значений
+        for (int i = 1; i < theaterSeating.length; i++) {
+            theaterSeating[i] = i;
+        }
+
+        // присваиваем текущее значение и увеличиваем счётчик
+
+
+        // Выводим на экран элемент во второй строке и первом столбце (индекс [1][0])
+
+    }
+}
