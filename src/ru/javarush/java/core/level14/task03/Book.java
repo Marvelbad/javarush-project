5b68c614477eb23f21cdb06611ab18630447d0d6
Marvelbad <badra.aqua@gmail.com>
2025-10-03 23:46:24 +0300

    Task 03

---
+++ src/ru/javarush/java/core/level14/task03/Book.java
@@ -0,0 +1,11 @@
+package ru.javarush.java.core.level14.task03;
+
+
+public class Book {
+    String title;
+    int pages;
+
+    void printInfo() {
+        System.out.printf("Название: %s, страниц: %d%n", title, pages);
+    }
+}
