be3c95d75555f97d96a867d7e65bd37a719bb39d
Marvelbad <badra.aqua@gmail.com>
2025-10-04 22:00:09 +0300

    Task 17

---
+++ src/ru/javarush/java/core/level14/task17/Animal.java
@@ -0,0 +1,6 @@
+package ru.javarush.java.core.level14.task17;
+
+public class Animal {
+     String animalName;
+     int animalAge;
+}
---
+++ src/ru/javarush/java/core/level14/task17/Solution.java
@@ -0,0 +1,11 @@
+package ru.javarush.java.core.level14.task17;
+
+public class Solution {
+    public static void main(String[] args) {
+        Animal wildAnimal = new Animal();
+
+
+        System.out.println(wildAnimal.animalName);
+        System.out.println(wildAnimal.animalAge);
+    }
+}
