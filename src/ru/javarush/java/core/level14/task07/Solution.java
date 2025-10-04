b0689381d0df48ea8a7fe75cdc9407445c280c09
Marvelbad <badra.aqua@gmail.com>
2025-10-04 12:55:12 +0300

    Task 07

---
+++ src/ru/javarush/java/core/level14/task07/Solution.java
@@ -0,0 +1,13 @@
+package ru.javarush.java.core.level14.task07;
+
+public class Solution {
+    public static void main(String[] args) {
+        Counter counter = new Counter();
+        counter.increment();
+        counter.increment();
+        counter.increment();
+        counter.printValue();
+    }
+}
+
+
