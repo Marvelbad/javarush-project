c0284084a71214dfd5dd6af96fa76d9c316cd7f4
Marvelbad <badra.aqua@gmail.com>
2025-10-04 22:12:27 +0300

    Task 20

---
+++ src/ru/javarush/java/core/level14/task20/Solution.java
@@ -0,0 +1,25 @@
+package ru.javarush.java.core.level14.task20;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем объект класса Student
+        Student newStudent = new Student();
+    }
+
+}
+    class Student {
+        int studentGrade = 1;
+
+        {
+            // Выводим текущую оценку
+            System.out.println("Блок 1: grade = " + studentGrade);
+            // Устанавливаем значение оценки в 5
+            studentGrade = 5;
+        }
+
+        // Второй блок инициализации
+        {
+            // Выводим текущую оценку
+            System.out.println("Блок 2: grade = " + studentGrade);
+        }
+    }
