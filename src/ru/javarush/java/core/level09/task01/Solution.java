f38803a25725f9dcdd452c52afb1f90bba6ede26
Marvelbad <badra.aqua@gmail.com>
2025-10-09 19:31:34 +0300

    Task 01

---
+++ src/ru/javarush/java/core/level09/task01/Solution.java
@@ -0,0 +1,11 @@
+package ru.javarush.java.core.level09.task01;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем строковую переменную для хранения пути
+        String path =   "C:\\Program Files\\Java";
+
+        // Выводим строку с экранированными обратными слэшами на консоль
+        System.out.println(path);
+    }
+}
