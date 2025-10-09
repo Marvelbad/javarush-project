1e13b52b90a7a50206857ad806446aadd444d7b7
Marvelbad <badra.aqua@gmail.com>
2025-10-09 19:22:53 +0300

    Task 19

---
+++ src/ru/javarush/java/core/level11/task19/Solution.java
@@ -0,0 +1,17 @@
+package ru.javarush.java.core.level11.task19;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        // Вызов метода calculateReportData из main
+        calculateReportData();
+    }
+
+    public static void calculateReportData() {
+        processRawNumbers();
+    }
+
+    public static void processRawNumbers() {
+        int res = 100 / 0;
+    }
+}
