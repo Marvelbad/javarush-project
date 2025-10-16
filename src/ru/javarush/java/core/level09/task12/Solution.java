7daa823be0019a3a3b07f02dbefc62636bf1da37
Marvelbad <badra.aqua@gmail.com>
2025-10-16 17:40:39 +0300

    Task 12

---
+++ src/ru/javarush/java/core/level09/task12/Solution.java
@@ -0,0 +1,29 @@
+package ru.javarush.java.core.level09.task12;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем объект Scanner для ввода данных с клавиатуры
+        Scanner console = new Scanner(System.in);
+
+        // Запрашиваем у пользователя ввод названия продукта
+        System.out.println("Введите название продукта:");
+        String productName = console.nextLine();
+
+        // Проверяем, начинается ли название с буквы 'E'
+        if (productName.startsWith("E")) {
+            // Извлекаем первые три символа строки
+            String substring = productName.substring(0, 3);
+            System.out.println(substring);
+        } else {
+            System.out.println("Первая буква не E");
+        }
+
+            // Выводим аббревиатуру на экран
+
+
+            // Выводим сообщение, если первая буква не 'E'
+
+    }
+}
