89b4052905fa1aa4d07021af15b03bc1a58c2e83
Marvelbad <badra.aqua@gmail.com>
2025-10-20 21:07:26 +0300

    Task 07

---
+++ src/ru/javarush/java/core/level15/task07/main/ModuleMain.java
@@ -0,0 +1,9 @@
+package ru.javarush.java.core.level15.task07.main;
+
+public class ModuleMain {
+    public static void main(String[] args) {
+        // Классы в одном пакете имеют доступ к package-private методам
+        ModuleHelper helper = new ModuleHelper();
+        helper.provideInternalAssistance();
+    }
+}
