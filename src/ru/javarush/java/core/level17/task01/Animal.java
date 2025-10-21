b13bd677c78ee65911043ef9adc27e5f77fef29a
Marvelbad <badra.aqua@gmail.com>
2025-10-21 22:16:08 +0300

    Task 17

---
+++ src/ru/javarush/java/core/level17/task01/Animal.java
@@ -0,0 +1,9 @@
+package ru.javarush.java.core.level17.task01;
+
+public class Animal {
+    String petName;
+
+    public void eat() {
+        System.out.println(petName + " ест.");
+    }
+}
---
+++ src/ru/javarush/java/core/level17/task01/Cat.java
@@ -0,0 +1,8 @@
+package ru.javarush.java.core.level17.task01;
+
+public class Cat extends Animal {
+
+    public void meow() {
+        System.out.println(petName + " говорит: Мяу!");
+    }
+}
