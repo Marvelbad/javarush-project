164f8f7f7b9dd3cafc168ff521a82086886fce2c
Marvelbad <badra.aqua@gmail.com>
2025-10-21 22:36:22 +0300

    Task 04

---
+++ src/ru/javarush/java/core/level17/task04/Fruit.java
@@ -0,0 +1,9 @@
+package ru.javarush.java.core.level17.task04;
+
+public class Fruit {
+    String fruitColor;
+
+    public void printColor() {
+        System.out.println(fruitColor + ": цвет фрукта.");
+    }
+}
