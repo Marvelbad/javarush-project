c264c432ce8ee9ea5e544d1eb9c2cbd9bf6002f4
Marvelbad <badra.aqua@gmail.com>
2025-10-22 21:39:46 +0300

    Task 05

---
+++ src/ru/javarush/java/core/level18/task05/MessagePrinter.java
@@ -0,0 +1,12 @@
+package ru.javarush.java.core.level18.task05;
+
+public class MessagePrinter {
+
+    public void printMessage(String text) {
+        System.out.println(text);
+    }
+
+    public void printMessage(int a, int b) {
+        System.out.println(a + b);
+    }
+}
