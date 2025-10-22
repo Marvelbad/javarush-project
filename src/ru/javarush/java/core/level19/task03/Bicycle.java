5a2522d0bdec2f4ada893c3fd9b81bd351226352
Marvelbad <badra.aqua@gmail.com>
2025-10-23 00:09:47 +0300

    Task 03

---
+++ src/ru/javarush/java/core/level19/task03/Bicycle.java
@@ -0,0 +1,9 @@
+package ru.javarush.java.core.level19.task03;
+
+public class Bicycle extends Vehicle {
+
+    @Override
+    public void move() {
+        System.out.println("Велосипед крутит педали");
+    }
+}
---
+++ src/ru/javarush/java/core/level19/task03/Vehicle.java
@@ -0,0 +1,6 @@
+package ru.javarush.java.core.level19.task03;
+
+public abstract class Vehicle {
+
+    public abstract void move();
+}
