b0979c99a5c69f4907bfbf7bde0224b103ade163
Marvelbad <badra.aqua@gmail.com>
2025-10-22 23:24:52 +0300

    Task 12

---
+++ src/ru/javarush/java/core/level18/task12/Cat.java
@@ -0,0 +1,10 @@
+package ru.javarush.java.core.level18.task12;
+
+public class Cat extends Animal {
+
+    @Override
+    public void makeSound() {
+        super.makeSound();
+        System.out.println("Мяу!");
+    }
+}
