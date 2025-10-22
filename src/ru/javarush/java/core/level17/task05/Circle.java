f0a2246507ddfe21799acb733b65048f27622b51
Marvelbad <badra.aqua@gmail.com>
2025-10-22 14:09:50 +0300

    Task 05

---
+++ src/ru/javarush/java/core/level17/task05/Circle.java
@@ -0,0 +1,8 @@
+package ru.javarush.java.core.level17.task05;
+
+public class Circle extends Shape {
+    @Override
+    void draw() {
+        System.out.println("Drawing circle");
+    }
+}
---
+++ src/ru/javarush/java/core/level17/task05/Shape.java
@@ -0,0 +1,8 @@
+package ru.javarush.java.core.level17.task05;
+
+public class Shape {
+
+    void draw() {
+        System.out.println("Drawing shape");
+    }
+}
