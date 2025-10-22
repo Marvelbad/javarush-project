7a38d4c5c743f6ac15eef812e7c7bea06dd95344
Marvelbad <badra.aqua@gmail.com>
2025-10-22 20:04:02 +0300

    Task 13

---
+++ src/ru/javarush/java/core/level17/task13/Animal.java
@@ -0,0 +1,9 @@
+package ru.javarush.java.core.level17.task13;
+
+public class Animal {
+    public String petName;
+
+    public void eat() {
+        System.out.println(petName + " ест.");
+    }
+}
