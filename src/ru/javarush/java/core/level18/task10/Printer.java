6a1b739002e6f4d2c93dc5f6fc92e69742143761
Marvelbad <badra.aqua@gmail.com>
2025-10-22 23:20:27 +0300

    Task 10

---
+++ src/ru/javarush/java/core/level18/task10/Printer.java
@@ -0,0 +1,12 @@
+package ru.javarush.java.core.level18.task10;
+
+public class Printer {
+
+    public void print(int a) {
+        System.out.println("Число: " + a);
+    }
+
+    public void print(String str) {
+        System.out.println("Строка: " + str);
+    }
+}
