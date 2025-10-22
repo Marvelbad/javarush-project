1f8c299f78aba14ea1c8fa5318b9a3b62adc97db
Marvelbad <badra.aqua@gmail.com>
2025-10-22 21:43:12 +0300

    Task 04

---
+++ src/ru/javarush/java/core/level18/task04/Bicycle.java
@@ -0,0 +1,9 @@
+package ru.javarush.java.core.level18.task04;
+
+public class Bicycle extends Vehicle {
+
+    @Override
+    public void move() {
+        System.out.println("Велосипед едет.");
+    }
+}
