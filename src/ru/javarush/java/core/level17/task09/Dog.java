16853fd125a0a5827a9fd794a4720e5c57751849
Marvelbad <badra.aqua@gmail.com>
2025-10-22 16:58:47 +0300

    Task 09

---
+++ src/ru/javarush/java/core/level17/task09/Dog.java
@@ -0,0 +1,10 @@
+package ru.javarush.java.core.level17.task09;
+
+public class Dog  extends Animal{
+
+    @Override
+    public void makeSound() {
+        super.makeSound();
+        System.out.println("Собака лает");
+    }
+}
