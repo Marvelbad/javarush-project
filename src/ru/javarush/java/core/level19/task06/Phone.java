8e5f9c01292bd4c47bc4c4043f2403e9315a41c3
Marvelbad <badra.aqua@gmail.com>
2025-10-23 11:08:54 +0300

    Task 06

---
+++ src/ru/javarush/java/core/level19/task06/Phone.java
@@ -0,0 +1,9 @@
+package ru.javarush.java.core.level19.task06;
+
+public class Phone extends Device {
+
+    @Override
+    public void turnOn() {
+        System.out.println("Телефон включен");
+    }
+}
