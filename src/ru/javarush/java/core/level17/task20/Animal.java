5dde885ddb4996b2ba36877fac823e6258034712
Marvelbad <badra.aqua@gmail.com>
2025-10-23 11:03:00 +0300

    Task 20

---
+++ src/ru/javarush/java/core/level17/task20/Animal.java
@@ -0,0 +1,13 @@
+package ru.javarush.java.core.level17.task20;
+
+public class Animal {
+
+    public void makeSound() {
+        System.out.println("Some sound");
+    }
+
+    public void sleep() {
+        System.out.println("Animal is going to sleep...");
+        this.makeSound();
+    }
+}
