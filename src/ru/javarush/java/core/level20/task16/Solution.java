7a16bf5c59b0ab372cfc3e35e0130d08cdd97afa
Marvelbad <badra.aqua@gmail.com>
2025-10-29 21:02:19 +0300

    Task 16

--- src/ru/javarush/java/core/level20/task16/Solution.java
+++ src/ru/javarush/java/core/level20/task16/Solution.java
@@ -1,18 +1,16 @@
 package ru.javarush.java.core.level20.task16;
 
-import java.util.function.Supplier;
-
 public class Solution {
     public static void main(String[] args) {
         // Лямбда-реализация: преобразует строку к нижнему регистру
-        StringTransFormer transFormer = s -> s.toLowerCase();
+        StringTransformer transFormer = s -> s.toLowerCase();
 
         // Вызываем default-метод для строки "JAVA ROCKS"
         transFormer.printTransformed("JAVA ROCKS");
     }
 
     @FunctionalInterface
-    interface StringTransFormer {
+    interface StringTransformer {
         String transform(String s);
 
         default void printTransformed(String s) {
