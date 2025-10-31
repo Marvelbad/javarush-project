1eb28750c92b927cadc86bae9d6e2e43f68c66bf
Marvelbad <badra.aqua@gmail.com>
2025-10-31 16:11:04 +0300

    Task 13

--- src/ru/javarush/java/core/level21/task13/StringChecker.java
+++ src/ru/javarush/java/core/level21/task13/StringChecker.java
@@ -2,7 +2,7 @@ package ru.javarush.java.core.level21.task13;
 
 public interface StringChecker {
     static boolean isEmpty(String text) {
-        if (text == null || text.length() == 0) {
+        if (text == null || text.isEmpty()) {
             return true;
         }
         return false;
