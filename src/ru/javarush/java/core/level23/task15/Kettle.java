21dab4d96b538f8b710ba735afa0a520d18a8734
Marvelbad <badra.aqua@gmail.com>
2025-11-04 13:17:25 +0300

    Task 15

---
+++ src/ru/javarush/java/core/level23/task15/Kettle.java
@@ -0,0 +1,12 @@
+package ru.javarush.java.core.level23.task15;
+
+public class Kettle extends Appliance {
+    @Override
+    public void turnOn() {
+
+    }
+
+    public void boil() {
+        System.out.println("Вода закипела");
+    }
+}
