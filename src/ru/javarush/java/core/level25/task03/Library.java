f0229f0bc684b140f11a95811945253f10fb894a
Marvelbad <badra.aqua@gmail.com>
2025-11-06 15:06:20 +0300

    Task 03

---
+++ src/ru/javarush/java/core/level25/task03/Library.java
@@ -0,0 +1,9 @@
+package ru.javarush.java.core.level25.task03;
+
+public class Library {
+    static class Archive {
+        public static void readParchment() {
+            System.out.println("Древний свиток разгадан!");
+        }
+    }
+}
