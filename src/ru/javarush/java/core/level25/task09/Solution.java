7f2ffce146381dcc8854f0aa5e6f3e1edb29c787
Marvelbad <badra.aqua@gmail.com>
2025-11-06 20:15:44 +0300

    Task 09

---
+++ src/ru/javarush/java/core/level25/task09/Solution.java
@@ -0,0 +1,8 @@
+package ru.javarush.java.core.level25.task09;
+
+public class Solution {
+    public static void main(String[] args) {
+        SystemCore systemCore = new SystemCore();
+        SystemCore systemCore1 = new SystemCore();
+    }
+}
