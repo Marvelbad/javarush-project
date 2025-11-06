54c38be85996af76c91f3ae8d4a0e7eb49163144
Marvelbad <badra.aqua@gmail.com>
2025-11-06 15:01:59 +0300

    Task 02

---
+++ src/ru/javarush/java/core/level25/task02/Animal.java
@@ -0,0 +1,9 @@
+package ru.javarush.java.core.level25.task02;
+
+public class Animal {
+    protected String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+}
---
+++ src/ru/javarush/java/core/level25/task02/Cat.java
@@ -0,0 +1,8 @@
+package ru.javarush.java.core.level25.task02;
+
+public class Cat extends Animal {
+
+    public Cat(String name) {
+        super(name);
+    }
+}
